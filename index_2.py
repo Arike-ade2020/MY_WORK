93ea2093da4f402246aaa2402c721722e500dd25
Arike-ade2020 <mansuratabdulraheem2018@gmail.com>
2021-05-31 11:48:19 +0100

    added somethings to my file

---
+++ index_2.py
@@ -0,0 +1,8 @@
+names = {'john', 'jacob', 'anabel', 'jonathan', 'jack', 'tabita', 'winfred', 'jalong',}
+
+#for names that start with 'j'
+for i in names: 
+    if i.startwith('j'): 
+        i == True 
+        print ('its {} that {} begins with a "j"' .format(True, i), 
+        print ("    ")
