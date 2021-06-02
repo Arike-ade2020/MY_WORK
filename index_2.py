c3a71500f166989a8ae94ecba52d0ef73facd90c
Arike-ade2020 <mansuratabdulraheem2018@gmail.com>
2021-06-02 11:59:43 +0100

    my first line

--- index_2.py
+++ index_2.py
@@ -2,7 +2,19 @@ names = {'john', 'jacob', 'anabel', 'jonathan', 'jack', 'tabita', 'winfred', 'ja
 
 #for names that start with 'j'
 for i in names: 
-    if i.startwith('j'): 
+    if i .startswith('j'): 
         i == True 
-        print ('its {} that {} begins with a "j"' .format(True, i), 
-        print ("    ")
+        print ('its {} that {} begins with a "j"' .format(True, i))
+    else: 
+        i == False
+        print ('its {} that {} begins with a "j"' .format(False, i))
+# FOR LONGEST WORD 
+print ('the longestword is {}' .format (max(names, key=len)))
+#FOR SORT AND INDEX 
+names.sort()
+for idx, val in enumerate(names):
+    print ('{} has an index of {}' .format(val,idx))
+
+# for lenth of words 
+#for i in names:
+    #print('{} = {}'.format(i,len(i),))
